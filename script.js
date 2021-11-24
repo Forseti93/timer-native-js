66040722cbd301de5b7d91c642ea82b9dcf77031
Sergey <sergeylavrenov93@gmail.com>
2021-11-24 22:10:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+//установить дедлайн YYYY-MM-DD
+const timerUntil = "2021-11-28";
+
+const timerComponents = [
+  document.querySelector(".timer>p>.days"),
+  document.querySelector(".timer>p>.hours"),
+  document.querySelector(".timer>p>.minutes"),
+  document.querySelector(".timer>p>.seconds"),
+];
+
+//функция, получить оставшееся время (между сейчас и дедлайном) день, час, минута, секунда
+function remainingTime(deadline) {
+  const dateNow = Date.parse(Date());
+  const remainingTimeMS = Date.parse(deadline) - dateNow;
+  const days = Math.floor(remainingTimeMS / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((remainingTimeMS / (1000 * 60 * 60 * 24)) % 24);
+  const minutes = Math.floor((remainingTimeMS / (1000 * 60)) % 60);
+  const seconds = Math.floor((remainingTimeMS / 1000) % 60);
+
+  return {
+    remainingTime: remainingTimeMS,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
+
+function loopAndReplace(timerUnits, timerValues) {
+  const objValuesArray = Object.values(timerValues);
+  for (let i = 1; i < objValuesArray.length; i++) {
+    if (objValuesArray[i] / 10 < 1) {
+      timerUnits[i - 1].innerText = `0${objValuesArray[i]}`;
+    } else {
+      timerUnits[i - 1].innerText = objValuesArray[i];
+    }
+  }
+}
+
+const second = setInterval(function () {
+  loopAndReplace(timerComponents, remainingTime(timerUntil));
+}, 1000);
+
+if (remainingTime(timerUntil).remainingTime === 0) {
+  clearInterval(second);
+}
